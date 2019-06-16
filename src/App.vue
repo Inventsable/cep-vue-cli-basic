369dca6859354394068c9df5c5f9ae301d43536b
Tom Scharstein <trscharstein@gmail.com>
2019-06-16 16:38:08 -0700

    Removing CSInterface dependency from utilities

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,28 @@ export default {
   components: {
     HelloWorld
   },
+  data: () => ({
+    csInterface: null,
+    identity: null,
+    stylizer: null,
+    menus: null,
+    isMounted: false
+  }),
+  mounted() {
+    console.clear();
+    this.csInterface = new CSInterface();
+    this.csInterface.addEventListener("console", this.consoler);
+
+    // Utility components are already mounted prior to this
+    console.log(
+      `${this.identity.extName} ${this.identity.extVersion} : ${
+        this.identity.isDev ? "DEV" : "BUILD"
+      }`
+    );
+    this.isMounted = true;
+
+    this.loadUniversalScripts();
+  },
   methods: {
     dispatchEvent(name, data) {
       var event = new CSEvent(name, "APPLICATION");
@@ -21,6 +43,18 @@ export default {
     },
     loadScript(path) {
       this.csInterface.evalScript(`$.evalFile('${path}')`);
+    },
+    loadUniversalScripts() {
+      // Preloads universal scripts and main host script file
+      this.loadScript(`${this.identity.root}/src/host/universal/json2.jsx`);
+      this.loadScript(`${this.identity.root}/src/host/universal/Console.jsx`);
+      this.loadScript(
+        `${this.identity.root}/src/host/${this.identity.appName}/host.jsx`
+      );
+    },
+    consoler(msg) {
+      // Catches all console.log() usage in .jsx files via CSEvent
+      console.log(`${this.identity.appName}: ${msg.data}`);
     },
     getCSS(prop) {
       // Returns current value of CSS variable
